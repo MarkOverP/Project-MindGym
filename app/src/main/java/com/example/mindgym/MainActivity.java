ef894bfea55faa234e5ae6736d11e4142b17f95a
Sanya <markevan2545@gmail.com>
2023-11-23 23:46:56 +0700

    main page now finish

--- app/src/main/java/com/example/mindgym/MainActivity.java
+++ app/src/main/java/com/example/mindgym/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.mindgym;
 
+import android.os.Bundle;
+import android.widget.GridView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        GridView gv = (GridView)findViewById(R.id.GView);
+        gv.setAdapter(new ImgAdapterMainPage(this));
     }
 }
