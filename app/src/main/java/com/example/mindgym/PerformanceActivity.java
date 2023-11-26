f5355ac35faeb83070962d65eadd88d9588bfade
Puttipong2566 <bookkeehohojoko@hotmail.com>
2023-11-26 18:40:11 +0700

    Test233

--- app/src/main/java/com/example/mindgym/PerformanceActivity.java
+++ app/src/main/java/com/example/mindgym/PerformanceActivity.java
@@ -2,25 +2,32 @@ package com.example.mindgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PerformanceActivity extends AppCompatActivity {
+    ImageView imageView9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_performance)                 ;
 
+        imageView9 = (ImageView) findViewById(R.id.imageView9);
+
+        imageView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPerfomance2Activity();
+            }
+        });
 
 
-
-
-
-
-
-
-
-
-
+    }
+    public void openPerfomance2Activity(){
+        Intent intente = new Intent( this, Performance2Activity.class);
+        startActivity(intente);
     }
 }
