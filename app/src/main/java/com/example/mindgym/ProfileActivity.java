4d031a3011d944904698773cbe345ae50dc6c29b
Puri <Dettanawiwat@hotmail.com>
2023-11-26 17:12:04 +0700

    link navigation game , performance , profile

--- app/src/main/java/com/example/mindgym/ProfileActivity.java
+++ app/src/main/java/com/example/mindgym/ProfileActivity.java
@@ -1,8 +1,13 @@
 package com.example.mindgym;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +15,37 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigator);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.performance:
+                        startActivity(new Intent(getApplicationContext(), PerformanceActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.ItGame:
+                        startActivity(new Intent(getApplicationContext(), GameActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
