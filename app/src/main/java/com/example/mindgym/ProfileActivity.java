8ce54fb5cf753bcf1bf40e12fd7fb6135ddf782f
Sanya <markevan2545@gmail.com>
2023-11-26 17:31:07 +0700

    profile setting button

--- app/src/main/java/com/example/mindgym/ProfileActivity.java
+++ app/src/main/java/com/example/mindgym/ProfileActivity.java
@@ -6,15 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView settingbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        settingbtn = (ImageView)findViewById(R.id.settingbtn) ;
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigator);
@@ -45,6 +53,13 @@ public class ProfileActivity extends AppCompatActivity {
                         return true;
                 }
                 return false;
+            }
+        });
+        settingbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentstpage = new Intent(this, SettingprofileActivity.class);
+                startActivity(intentstpage);
             }
         });
     }
