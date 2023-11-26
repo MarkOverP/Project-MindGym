299469df64b72435785be7bb46cb97165b5b3c70
Sanya <markevan2545@gmail.com>
2023-11-26 16:56:14 +0700

    setting icon in profile

--- app/src/main/java/com/example/mindgym/ScoreActivity.java
+++ app/src/main/java/com/example/mindgym/ScoreActivity.java
@@ -8,10 +8,23 @@ import android.widget.Button;
 
 public class ScoreActivity extends AppCompatActivity {
 
+    Button BackButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
+        BackButton = (Button) findViewById(R.id.BackButton);
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGameActivity();
+            }
+        });
+    }
+
+    public void openGameActivity(){
+        Intent intentbacktg = new Intent(this, GameActivity.class);
+        startActivity(intentbacktg);
     }
 }
